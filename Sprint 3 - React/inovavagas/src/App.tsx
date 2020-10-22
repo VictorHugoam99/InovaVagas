9f38b681ba2e6d47e232c6c01b0d17049b2e5da3
sug <felipesugisawa1@gmail.com>
2020-10-22 19:40:02 -0300

    graphic

--- Sprint 3 - React/inovavagas/src/App.tsx
+++ Sprint 3 - React/inovavagas/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Routers from './routers'
+// import { } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
+      <h1>Home</h1>
       <Routers/>
     </div>
   );
--- Sprint 3 - React/inovavagas/src/index.tsx
+++ Sprint 3 - React/inovavagas/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'; /* The following line can be included in your src/index.js or App.js file*/
 
 ReactDOM.render(
   <React.StrictMode>
